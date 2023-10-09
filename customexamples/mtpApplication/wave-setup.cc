b4964acbbd26561de89d392d97c375f63d0de0df
Ashutosh Sharma <ash2003sharma@gmail.com>
2023-10-10 01:34:37 +0530

    2023-10-10 01:34:37 commited via bash

--- customexamples/mtpApplication/wave-setup.cc
+++ customexamples/mtpApplication/wave-setup.cc
@@ -11,27 +11,48 @@ NetDeviceContainer WaveSetup::ConfigureDevices (NodeContainer& nodes)
   /*
     Setting up WAVE devices. With PHY & MAC using default settings. 
   */
-  YansWifiChannelHelper waveChannel = YansWifiChannelHelper::Default ();
-  YansWavePhyHelper wavePhy =  YansWavePhyHelper::Default ();
-  wavePhy.SetChannel (waveChannel.Create ());
+
+    
+
+ // The below set of helpers will help us to put together the wifi NICs we want
+  YansWifiChannelHelper wifiChannel = YansWifiChannelHelper();
+  wifiChannel.SetPropagationDelay ("ns3::ConstantSpeedPropagationDelayModel");
+  // wifiChannel.AddPropagationLoss ("ns3::RangePropagationLossModel", "MaxRange", DoubleValue(500));
+  wifiChannel.AddPropagationLoss ("ns3::FriisPropagationLossModel", "Frequency", DoubleValue(5.9e9));
+
+  YansWavePhyHelper wavePhy = YansWavePhyHelper::Default ();
+  Ptr<YansWifiChannel> channel = wifiChannel.Create ();
+  wavePhy.SetChannel (channel);
+  uint32_t channelID = channel->GetId ();
+
+  // Physical Layer parameters settings
+  wavePhy.Set("Frequency", UintegerValue(channelFrequency));
+  std::string channelTuple = "{" + std::to_string(channelID) + ", " + std::to_string(channelBandwidth) + ", " + channelBand + ", 0}";
+  wavePhy.Set("ChannelSettings", StringValue(channelTuple));
+
+  // ns-3 supports generate a pcap trace
   wavePhy.SetPcapDataLinkType (WifiPhyHelper::DLT_IEEE802_11_RADIO);
-  wavePhy.Set ("TxPowerStart", DoubleValue (5) );
-  wavePhy.Set ("TxPowerEnd", DoubleValue (33) );
-  wavePhy.Set ("TxPowerLevels", UintegerValue (8));
-  //Setup up MAC
   QosWaveMacHelper waveMac = QosWaveMacHelper::Default ();
-  WaveHelper waveHelper = WaveHelper::Default ();
+
+  Wifi80211pHelper waveHelper = Wifi80211pHelper::Default ();
 
   waveHelper.SetRemoteStationManager ("ns3::ConstantRateWifiManager",
-  						"DataMode", StringValue ("OfdmRate6MbpsBW10MHz"	),
-  						"ControlMode",StringValue ("OfdmRate6MbpsBW10MHz"),
+  						"DataMode", StringValue ("OfdmRate3MbpsBW10MHz"	),
+  						"ControlMode",StringValue ("OfdmRate3MbpsBW10MHz"), // Change to 1 Mbps
   						"NonUnicastMode", StringValue ("OfdmRate6MbpsBW10MHz"));
-
   NetDeviceContainer devices = waveHelper.Install (wavePhy, waveMac, nodes);
 
-
-  //if you want PCAP trace, uncomment this!
-  //wavePhy.EnablePcap ("custom-application", devices); //This generates *.pcap files
+  for(int iNode=0;iNode<devices.GetN();iNode++){
+    Ptr<WifiNetDevice> node = DynamicCast<WifiNetDevice> (devices.Get(iNode));
+    Ptr<WifiPhy> nodePhy = node->GetPhy();
+    // Set Tx Power to 500m
+    nodePhy->SetTxPowerStart(19.9); 
+    nodePhy->SetTxPowerEnd(19.9);
+    // Set CSR to 700m
+    nodePhy->SetCcaEdThreshold(-84.87);
+    // Set RX Sensitivity 
+    nodePhy->SetRxSensitivity(-84.87);
+  }
 
   return devices;
 }
--- customexamples/mtpApplication/wave-setup.h
+++ customexamples/mtpApplication/wave-setup.h
@@ -1,8 +1,20 @@
 #ifndef WAVE_SETUP_H
 #define WAVE_SETUP_H
 #include "ns3/core-module.h"
+#include "ns3/vector.h"
+#include "ns3/string.h"
+#include "ns3/double.h"
+#include "ns3/config.h"
+#include "ns3/log.h"
+#include "ns3/yans-wifi-helper.h"
 #include "ns3/wave-module.h"
-#include "ns3/network-module.h"
+#include "ns3/yans-wifi-channel.h"
+#include <iostream>
+#include "iomanip"
+#include "ns3/ocb-wifi-mac.h"
+#include "ns3/wifi-80211p-helper.h"
+#include "ns3/wave-mac-helper.h"
+#include "ns3/wave-module.h"
 
 namespace ns3
 {
@@ -10,6 +22,12 @@ namespace ns3
  */
   class WaveSetup
   {
+    private:
+      int numPacket = 1;
+      int channelFrequency = 5900;
+      uint16_t channelBandwidth = 10;
+      std::string channelBand = "BAND_5GHZ";
+
     public:
       WaveSetup ();
       virtual ~WaveSetup ();
