7160c88d9003a226aa513e14c037900bb1a13718
Sukhija-Aniket <b20081@students.iitmandi.ac.in>
2023-11-22 15:34:05 +0530

    commited via bash

--- customexamples/CustomApplicationExample/wave-setup.cc
+++ customexamples/CustomApplicationExample/wave-setup.cc
@@ -34,24 +34,27 @@ NetDeviceContainer WaveSetup::ConfigureDevices (NodeContainer& nodes, bool enabl
 
   NetDeviceContainer devices = waveHelper.Install (wavePhy, waveMac, nodes);
 
+
+
   for(uint32_t iNode=0;iNode<devices.GetN();iNode++){
     Ptr<WaveNetDevice> node = DynamicCast<WaveNetDevice> (devices.Get(iNode));
-
+    uint32_t retryLimit = 2;
+    node->GetRemoteStationManager()->SetMaxSlrc(retryLimit);
+    node->GetRemoteStationManager()->SetMaxSsrc(retryLimit);
     // Change AIFs[1] = 3; AIFs[0] = 2; cw_min = 15; cw_max = 31
-    // Ptr<OcbWifiMac> nodeMac = node->GetMac(178);
-    // Ptr<QosTxop> viTxop = nodeMac->GetQosTxop(AC_VI);
-    // Ptr<QosTxop> voTxop = nodeMac->GetQosTxop(AC_VO);
-    // viTxop->SetMinCw(15);
-    // viTxop->SetMaxCw(31);
-    // viTxop->SetAifsn(3);
-    // voTxop->SetAifsn(2);
+    Ptr<OcbWifiMac> nodeMac = node->GetMac(178);
+    Ptr<QosTxop> viTxop = nodeMac->GetQosTxop(AC_VI);
+    Ptr<QosTxop> voTxop = nodeMac->GetQosTxop(AC_VO);
+    viTxop->SetMinCw(15);
+    viTxop->SetMaxCw(31);
+    viTxop->SetAifsn(3);
+    voTxop->SetAifsn(2);
 
     Ptr<WifiPhy> nodePhy = node->GetPhy(0);
-    // Set SIFS = 32*10^(-6) seconds and Slot Time = 13*10^(-6) seconds
-    // Time sifs = Time::FromInteger(32, Time::US);
-    // nodePhy->SetSifs(sifs);
-    // Time slot = Time::FromInteger(13, Time::US);
-    // nodePhy->SetSlot(slot);
+    Time sifs = Time::FromInteger(32, Time::US);
+    nodePhy->SetSifs(sifs);
+    Time slot = Time::FromInteger(13, Time::US);
+    nodePhy->SetSlot(slot);
 
     // Set Tx Power to 500m
     nodePhy->SetTxPowerStart(19.9);
@@ -66,9 +69,6 @@ NetDeviceContainer WaveSetup::ConfigureDevices (NodeContainer& nodes, bool enabl
     int numNodes = devices.GetN();
     wavePhy.EnablePcap("pcaps/static-node-delay-calc-n" + std::to_string(numNodes), devices);
   }
-  //if you want PCAP trace, uncomment this!
-  //wavePhy.EnablePcap ("custom-application", devices); //This generates *.pcap files
-
   return devices;
 }
 
