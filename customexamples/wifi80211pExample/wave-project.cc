5cd521f0141ca1b2bbd0771277c5e0a52298e150
Sukhija-Aniket <b20081@students.iitmandi.ac.in>
2024-02-02 16:52:21 +0530

    commited via bash

--- customexamples/wifi80211pExample/wave-project.cc
+++ customexamples/wifi80211pExample/wave-project.cc
@@ -2,8 +2,8 @@
 #include "ns3/wifi-module.h"
 #include "ns3/mobility-module.h"
 #include "ns3/core-module.h"
-#include "custom-application.h"
-#include "custom-mobility-model.h"
+#include "ns3/custom-application.h"
+#include "ns3/custom-mobility-model.h"
 #include "ns3/trace-functions.h"
 #include "ns3/functions.h"
 
@@ -22,9 +22,6 @@ NetDeviceContainer ConfigureDevices(NodeContainer &nodes, vector<vector<DisplayO
   YansWavePhyHelper wavePhy =  YansWavePhyHelper::Default ();
   Ptr<YansWifiChannel> channel = waveChannel.Create();
   wavePhy.SetChannel (channel);
-  // wavePhy.Set("Frequency", UintegerValue(5900));
-  // std::string channelTuple = "{" + std::to_string(channelID) + ", " + std::to_string(10) + ", BAND_5GHZ, 0}";
-  // wavePhy.Set("ChannelSettings", StringValue(channelTuple));
 
   wavePhy.SetPcapDataLinkType (WifiPhyHelper::DLT_IEEE802_11_RADIO);
   //Setup up MAC
@@ -37,16 +34,12 @@ NetDeviceContainer ConfigureDevices(NodeContainer &nodes, vector<vector<DisplayO
   						"NonUnicastMode", StringValue ("OfdmRate3MbpsBW10MHz"),
               "MaxSlrc", UintegerValue(2),
               "MaxSsrc", UintegerValue(2));
-  // );
-  // wavePhy.EnablePcapAll("wave-project", true);
 
   NetDeviceContainer devices = waveHelper.Install (wavePhy, waveMac, nodes);
 
-
-
   for(uint32_t iNode=0;iNode<devices.GetN();iNode++){
     Ptr<WaveNetDevice> node = DynamicCast<WaveNetDevice> (devices.Get(iNode));
-    // Change AIFs[1] = 3; AIFs[0] = 2; cw_min = 15; cw_max = 31
+    // Note: Change AIFs[1] = 3; AIFs[0] = 2; cw_min = 15; cw_max = 31
     Ptr<OcbWifiMac> nodeMac = node->GetMac(178);
     Ptr<QosTxop> viTxop = nodeMac->GetQosTxop(AC_VI);
     Ptr<QosTxop> voTxop = nodeMac->GetQosTxop(AC_VO);
@@ -72,10 +65,6 @@ NetDeviceContainer ConfigureDevices(NodeContainer &nodes, vector<vector<DisplayO
     uint16_t band = nodePhy->GetPhyBand();
     NS_LOG_UNCOND("number: "<<number<<", channel: "<<channel<<", cw: "<<cw<<", band: "<<band);
     // (Aniket Sukhija)
-    // Time sifs = Time::FromInteger(32, Time::US);
-    // nodePhy->SetSifs(sifs);
-    // Time slot = Time::FromInteger(13, Time::US);
-    // nodePhy->SetSlot(slot);
 
     // Set Tx Power to 500m
     nodePhy->SetTxPowerStart(19.9);
@@ -192,9 +181,6 @@ int main (int argc, char *argv[])
   Simulator::Run();
   Simulator::Destroy();
 
-  // int sz = objContainers[MACTXBACKOFFNUM]->size();
-  // int sz2 = objContainers[MACENQUEUENUM]->size();
-  // cout<<"this is size: "<<sz <<" "<<sz2<<endl;
   string fileName = getCustomFileName (__FILE__, fileN + ".log");
   FILE* fp = freopen(fileName.c_str (), "w", stdout);
   getObjTrace(objContainers, MACENQUEUENUM, fp);
